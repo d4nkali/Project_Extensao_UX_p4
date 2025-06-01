324e125f4bc6c0b06db1db2d90277ee74ca5e618
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-05-31 22:36:54 -0300

    Adicionando site sem ux na main
    Adicionando site sem ux na main
    
    Co-Authored-By: João Vitor <143559938+JANzxz@users.noreply.github.com>
    Co-Authored-By: JoaoMagalhaes-hub <142951817+JoaoMagalhaes-hub@users.noreply.github.com>

--- site-sem-ux/src/App.jsx
+++ site-sem-ux/src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import InicialSemUx from './pages/InicalSemUx';
+import Navbar from './components/NavbarCagada'
+import Empresa from './pages/Empresa';
+import './App.css';
+
 
 const App = () => {
 
 	return (
 
-		<div>App</div>
+		<div>
 
-	);
+			<BrowserRouter>
+				{/* Aqui ficara a navbar */}
+				<Navbar />
 
-};
+				<Container className="my-4">
+					<Routes>
+						<Route path="/" element={<InicialSemUx />} />
+						<Route path='/a-empresa' element={<Empresa />} />
+					</Routes>
+				</Container>
+			</BrowserRouter>
+
+		</div>
+
+	)
+
+}
 
 export default App;
---
+++ site-sem-ux/src/pages/Caridade.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Caridade = () => {
+	return (
+		<div>Caridade</div>
+	)
+}
+
+export default Caridade
